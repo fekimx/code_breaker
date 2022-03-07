8b98df3eba4e3df92b4d1b0e22a2b6a84918da7a
Sean Bollin <sean@sean-bollin.com>
2022-03-07 09:40:43 -0800

    Changing project to use React on the homepage

---
+++ coding/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import axios from 'axios';
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+const App = () => {
+  return (
+    <div>Code Breaker React App Hello World</div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    coding: './coding/src/index.js',
+  },
+  output: {
+    path: path.resolve('./coding/static/coding/'),
+    filename: '[name]-[fullhash].js',
+    publicPath: 'static/coding/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleTracker({
+      path: __dirname,
+      filename: './webpack-stats.json',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
