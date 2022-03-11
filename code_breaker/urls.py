1e18ae11e81f46f8e0ecd99bd3afe0c42a6effd1
Sean Bollin <sean@sean-bollin.com>
2022-03-11 11:54:36 -0800

    Adding REST API for logging in

--- code_breaker/urls.py
+++ code_breaker/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("coding.urls"))
+    path("", include("coding.urls")),
 ]
--- coding/src/App.js
+++ coding/src/App.js
@@ -1,12 +1,40 @@
 import './App.css';
 import axios from 'axios';
+import { Provider } from "react-redux";
+import store, { persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import React from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
 
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 const App = () => {
   return (
-    <div>Code Breaker React App Hello World</div>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <BrowserRouter>
+          <div>
+            <Routes>
+              <Route exact path="/login" element={ <Login/> } />
+              <Route exact path="/register" element={ <Register/> } />
+              <Route exact path='/' element={<ProtectedRoute/>}>
+                <Route exact path='/' element={<Dashboard/>}/>
+              </Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   )
 }
 
