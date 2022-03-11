331450f368c11c52444da7bfdf3e25604bea2847
zeroender <worthington.brian@gmail.com>
2022-03-11 16:56:15 -0500

    Models

--- coding/models.py
+++ coding/models.py
@@ -4,3 +4,153 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+# User related models, teacher, learner, admin.
+# We could in theory also use a boolean or enum to differentiate but I'm using models here
+# For better clarity
+
+# Admin - Superuser who can authorize/create Teachers
+class Admin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def serialize(self):
+        return {
+            "username": self.username,
+            "name": self.get_full_name,
+            "email": self.email,
+            "active": self.is_active,
+            "type": "Admin"
+        }
+
+# Teacher - extends user
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    authorized = models.BooleanField(default=False)
+
+    def serialize(self):
+        return {
+            "username": self.username,
+            "name": self.get_full_name,
+            "email": self.email,
+            "authorized": self.authorized,
+            "active": self.is_active,
+            "type": "Teacher"
+        }
+
+# Learner - extends user
+class Learner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def serialize(self):
+        return {
+            "username": self.username,
+            "name": self.get_full_name,
+            "email": self.email,
+            "active": self.is_active,
+            "type": "Learner"
+        }
+
+# Class - data object to organize students
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    secretKey = models.CharField(max_length=25)
+    active = models.BooleanField(default=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    TAs = models.ManyToManyField(Teacher, related_name="TAs")
+    students = models.ManyToManyField(Learner)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "key": self.secretKey,
+            "teacher": self.teacher,
+        }
+
+# open questions
+# should we add a model for unit test?
+# sure.  Unit test - has dynamically created id, input, expected output and visible field
+class UnitTest(models.Model):
+    id = models.IntegerField
+    input = models.CharField(max_length=100)
+    expectedOutput = models.CharField(max_length=250)
+    visible = models.BooleanField
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "input": self.input,
+            "expectedOutput": self.expectedOutput,
+            "visible": self.visible,
+        }
+
+# should we add a model for solution?
+class Solution(models.Model):
+    id = models.IntegerField
+    code = models.TextField
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "code": self.code
+        }
+
+# Question - starting with just codeQuestion.  
+# Cosider adding base question type if/when we get to multiple choice and long form 
+# note: I'm still considering what it will mean for us to have the "public repo" version of questions.
+# maybe just a seperate model?
+
+class CodeQuestion(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    author = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
+    practice = models.BooleanField(default=False)
+    version = models.IntegerField(default=1)
+    solutions = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True)
+    unitTests = models.ForeignKey(UnitTest, on_delete=models.CASCADE, null=True)
+    
+    def serialize(self):
+        return {
+            "name": self.name,
+            "author": self.author,
+            "practice": self.practice,
+            "version": self.version
+        }
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    questions = models.ManyToManyField(CodeQuestion)
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "author": self.author
+        }
+
+# we need a way to individually determine "done" from a learner to a question
+# and we need a way to individually determine percentage completion from a learner to an assignment
+# thus, we have status.  Status can be applied to a question and represents the learner's progress.
+class Status(models.Model):
+    learner = models.ManyToManyField(Learner)
+    question = models.ManyToManyField(CodeQuestion)
+    complete = models.BooleanField(default=False)
+
+    def serialize(self):
+        return {
+            "learner": self.learner.name,
+            "question": self.question.name,
+            "complete": self.complete
+        }
+
+# Progress is the same thing but for an assignment
+class Progress(models.Model):
+    learner = models.ManyToManyField(Learner)
+    assignment = models.ManyToManyField(Assignment)
+    percent = models.IntegerField(default=0)
+
+    def serialize(self):
+        return {
+            "learner": self.learner.name,
+            "assignment": self.assignment.name,
+            "percent": self.percent
+        }
