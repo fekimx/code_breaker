631c6c0ee211a30c2e34190125e0c68bbf09159b
Freeman Smith <freemanwsmith3@gmail.com>
2022-03-14 13:30:47 -0500

    created dotenv file and local sqllite

--- code_breaker/local_settings.py
+++ code_breaker/local_settings.py
@@ -15,3 +15,10 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 DEBUG = True
+
+DATABASES = {
+     'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME':  os.path.join(BASE_DIR, 'db.sqlite3')
+     }
+}
