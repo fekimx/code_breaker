cf4ad95d01a2b594c17ab70c44190b9c801986d6
Freeman Smith <freemanwsmith3@gmail.com>
2022-03-14 21:31:36 -0500

    took out staticfiles dirs

--- code_breaker/settings.py
+++ code_breaker/settings.py
@@ -148,9 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS = [os.path.join(BASE_DIR,'coding/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
