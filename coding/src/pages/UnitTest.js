65be8f17e9d71f300cab7994003b8aa57711ae9b
zeroender <worthington.brian@gmail.com>
2022-03-20 14:49:53 -0400

    Adding navigation to app.js and stub pages with links

---
+++ coding/src/pages/UnitTest.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { useNavigate } from "react-router";
+  
+const UnitTest = () => {
+  const history = useNavigate();
+  
+  return (
+  <>
+     <h1 style={{color:"green"}}>Here is where we'll have UnitTests!</h1>
+     <button onClick={()=>history("/")}>Return to Dashboard</button>
+     <button onClick={()=>history("/about")}>About</button>
+     <button onClick={()=>history("/class")}>Class</button>
+     <button onClick={()=>history("/solution")}>Solution</button>
+     <button onClick={()=>history("/unittest")}>UnitTest</button>
+     <button onClick={()=>history("/about")}>About</button>
+     <button onClick={()=>history("/question")}>Peep some questions!</button>
+  </>
+  )
+};
+  
+export default UnitTest;
