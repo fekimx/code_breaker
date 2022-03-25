f15230c8dd54fa90aea0e2d7f192a4d90e4c2a9e
Sean Bollin <sean@sean-bollin.com>
2022-03-24 20:06:53 -0700

    Ability to add a class

--- coding/routers.py
+++ coding/routers.py
@@ -1,7 +1,7 @@
 # core/routers.py
 from rest_framework.routers import SimpleRouter
 from coding.viewsets import RunViewSet, UserViewSet
-from coding.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet, ClassViewSet
+from coding.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet, ClassViewSet, JoinClassViewSet
 
 # From https://dev.to/koladev/django-rest-authentication-cmh
 
@@ -20,6 +20,7 @@ routes.register(r'run', RunViewSet, basename='run')
 # CLASS
 
 routes.register(r'class', ClassViewSet, basename='class')
+routes.register(r'joinClass', JoinClassViewSet, basename='joinClass')
 
 urlpatterns = [
     *routes.urls
