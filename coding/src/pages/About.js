35b7a5fcf199a42f612ce1dd59b64f43fdc1ac3c
keithvandyck <keith.vandyck+github@gmail.com>
2022-03-26 16:39:42 -0400

    stop keith-bug on about page

--- coding/src/pages/About.js
+++ coding/src/pages/About.js
@@ -7,7 +7,6 @@ const About = () => {
   
   return (
   <>
-     <!-- start of nav bar -->
      <button onClick={()=>history("/")}>Return to Dashboard</button>
      <button onClick={()=>history("/about")}>About</button>
      <button onClick={()=>history("/contact")}>Contact Us</button>
@@ -16,7 +15,6 @@ const About = () => {
      <button onClick={()=>history("/unittest")}>UnitTest</button>
      <button onClick={()=>history("/about")}>About</button>
      <button onClick={()=>history("/question")}>Peep some questions!</button>
-     <!-- end of nav bar -->
      <h1 style={{color:"green"}}>What is code-breaker?</h1>
      <p><b>code-breaker</b> is the easiest way for high school computer science teachers to assign asynchronous after-school assignments.  By leveraging the existing content, teachers can quickly spin up lesson plans to augment in-class instruction... but the system is also flexible enough to allow changes to the questions or auto-grading unit-tests.</p>
      
--- coding/src/pages/Contact.js
+++ coding/src/pages/Contact.js
@@ -7,7 +7,6 @@ const ContactUs = () => {
   
   return (
   <>
-     <!-- start of nav bar -->
      <button onClick={()=>history("/")}>Return to Dashboard</button>
      <button onClick={()=>history("/about")}>About</button>
      <button onClick={()=>history("/contact")}>Contact Us</button>
@@ -16,7 +15,6 @@ const ContactUs = () => {
      <button onClick={()=>history("/unittest")}>UnitTest</button>
      <button onClick={()=>history("/about")}>About</button>
      <button onClick={()=>history("/question")}>Peep some questions!</button>
-     <!-- end of nav bar -->
      <h1 style={{color:"green"}}>Contact Us</h1>
 <script type="text/javascript">
 (function(d, t){
