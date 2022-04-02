18f265249f4a2dd7323ce72441737889a116bb6e
zeroender <worthington.brian@gmail.com>
2022-04-01 23:39:14 -0400

    Dynamic table for student classes added

---
+++ coding/src/pages/StudentClassTable.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import useSWR from 'swr';
+import {useSelector} from "react-redux";
+import {fetcher} from "../utils/axios";
+
+function StudentClassTable(){
+
+    const [displayData, updateDisplayData] = useState([]);
+
+    const account = useSelector((state) => state.auth.account);
+    const userId = account?.id;
+
+    const user = useSWR(`/api/user/${userId}/`, fetcher);
+
+    const fetchLatestClasses = () => {
+        axios.get(`/api/studentClass/?studentId=` + account?.id, {})
+        .then((response) => {
+            const newDisplayData = response.data.map((studentClass) => {
+                return(
+                    <tr key={studentClass.name}>
+                        <td>{studentClass.name}</td>
+                        <td>{studentClass.secretKey}</td>
+                        <td>
+                            {studentClass.assignments.length} assignments
+                        </td>
+                    </tr>
+                )
+            });
+            updateDisplayData(newDisplayData);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    const data = { classCode: "", teacherId: account?.id, fetchLatestClasses: fetchLatestClasses};
+
+    useEffect(() => {
+        fetchLatestClasses();
+    }, []);
+
+    return(
+        <div>
+            <table className="table-striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Secret Key</th>
+                        <th>Assignments</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { displayData }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+ 
+ export default StudentClassTable;
