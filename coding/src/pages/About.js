93be6ac83288b80d92e814157dd65e0b41a1af40
keithvandyck <keith.vandyck+github@gmail.com>
2022-04-01 22:39:52 -0400

    Updated about page to include navbar

--- coding/src/pages/About.js
+++ coding/src/pages/About.js
@@ -1,25 +1,20 @@
-
+import '../App.css';
 import React from 'react';
 import { useNavigate } from "react-router";
+import Navbar from "../components/navbar/Navbar";
   
 const About = () => {
   const history = useNavigate();
   
   return (
-  <>
-     <button onClick={()=>history("/")}>Return to Dashboard</button>
-     <button onClick={()=>history("/about")}>About</button>
-     <button onClick={()=>history("/contact")}>Contact Us</button>
-     <button onClick={()=>history("/class")}>Class</button>
-     <button onClick={()=>history("/solution")}>Solution</button>
-     <button onClick={()=>history("/unittest")}>UnitTest</button>
-     <button onClick={()=>history("/question")}>Peep some questions!</button>
-     <h1 style={{color:"green"}}>What is code-breaker?</h1>
-     <p><b>code-breaker</b> is the easiest way for high school computer science teachers to assign asynchronous after-school assignments.  By leveraging the existing content, teachers can quickly spin up lesson plans to augment in-class instruction... but the system is also flexible enough to allow changes to the questions or auto-grading unit-tests.</p>
-     
-
-  </>
-  )
+    <div>
+      <Navbar/>
+      <h1>About code-breaker</h1>
+      <p>code-breaker is the easiest way for high school computer science teachers to assign asynchronous after-school assignments.</p>
+      <p>By leveraging the existing content, teachers can quickly spin up lesson plans to augment in-class instruction...</p>
+      <p>but the system is also flexible enough to allow changes to the questions or auto-grading unit-tests.</p>
+    </div>
+  );
 };
   
 export default About;
