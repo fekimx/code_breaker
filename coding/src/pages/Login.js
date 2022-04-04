8e944d7fb7dad3cd9a3989cb5ec27d19a847ae30
Brian Wagner <39681903+BrianWagner-Github@users.noreply.github.com>
2022-04-04 19:17:45 -0400

    TaC Update
    Updated based on meeting feedback. Terms page added. Basic jargon which can be modified. Improved padding and shadowing for Login and Register Page. Can't register unless Terms box is checked.

--- coding/src/pages/Login.js
+++ coding/src/pages/Login.js
@@ -53,8 +53,8 @@ function Login() {
   });
 
   return (<div className="h-screen flex bg-gray-bg1"> <Navbar/>
+  <div class ="pad">
   <div class="container">
-  <div className="w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16">
     <h1 className="text-2xl font-medium text-primary mt-4 mb-12 text-center">
       Log in to your account
     </h1>
@@ -104,7 +104,7 @@ function Login() {
     </form>
   </div>
   </div>
-</div>
+  </div>
   );
 }
 
