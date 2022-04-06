b8a7deac2d519b1701a0f486d16b5df4226a77c8
fekimx <fatmarabias@gmail.com>
2022-04-06 10:48:55 -0400

    added py unit tests

--- coding/tests.py
+++ coding/tests.py
@@ -1,8 +1,11 @@
+import json
+from random import randint
 import unittest
 from django.test import LiveServerTestCase, TestCase
 from selenium import webdriver
 from os import getcwd
 import time
+from coding.models import Class
 
 class Hosttest(unittest.TestCase):
     
@@ -99,5 +102,17 @@ class Hosttest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
+class ModelsTest(TestCase):
+    def createClass(self, name, secretKey):
+        return Class.objects.create(name=name, secretKey=secretKey)
+    # silly test to make sure it is working!
+    def test_class(self):
+        class_obj = self.createClass(name='Class 1', secretKey=str(randint(10000,99999)))
+        self.assertTrue(isinstance(class_obj, Class))
+
+    def test_classCode(self):
+        data = json.load(open('./seed/0006_Class.json'))
+        self.assertIn("'secretKey': '001'", str(data))
+        
 if __name__=="__main__":
     unittest.main()
