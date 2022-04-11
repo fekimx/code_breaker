257aa4bd92679bb24ecaf4911798696ff28dbe64
Sean Bollin <sean@sean-bollin.com>
2022-04-10 19:17:06 -0700

    Dockerizing the application

--- migrate_and_seed_local.sh
+++ migrate_and_seed_local.sh
@@ -1,10 +1,11 @@
+# !/bin/bash
+
 # copied from https://medium.com/@ardho/migration-and-seeding-in-django-3ae322952111
 
-# !/bin/bash
-python3 manage.py migrate --settings=code_breaker.local_settings
+python manage.py migrate --settings=code_breaker.local_settings
 fixtures=$(ls seed/)
 while IFS= read -r fixture; do
     echo -n "Seeding "
     echo $fixture
-    python3 manage.py loaddata seed/$fixture --settings=code_breaker.local_settings
+    python manage.py loaddata seed/$fixture --settings=code_breaker.local_settings
 done <<< "$fixtures"
