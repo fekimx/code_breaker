9106f33ceb23b0dbb60859fb3ddd40fc7904b61a
keithvandyck <keith.vandyck+github@gmail.com>
2022-04-27 21:02:56 -0400

    Adding box around contact & about pages

--- coding/src/pages/Contact.js
+++ coding/src/pages/Contact.js
@@ -11,24 +11,17 @@ const Contact = () => {
     <div className="single-column">
     <NavHeader user="None" title="Contact" />
 
-  <div className="h-screen flex bg-gray-bg1">
-<div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '10vh'
-      }}
-    >
+    <div class="container">
+
       <h1>Contact Us</h1>
 
-    </div><div className="page-body">
+    <div className="page-body">
       <h2>Contact code-breaker.io</h2>
       <p>We would love to hear from you!  Please send all comments/questions to <a href="mailto:project.codebreaker@gmail.com">project.codebreaker@gmail.com</a>.</p>
 
     </div>
-    <Footer/>  
     </div>
+    <Footer/>  
     </div>
   );
 };
