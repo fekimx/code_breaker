db1eecb35632de56784c47f26f72348028badd7e
Brian Wagner <39681903+BrianWagner-Github@users.noreply.github.com>
2022-04-30 19:01:48 -0400

    Expansion

--- coding/src/pages/Legal.js
+++ coding/src/pages/Legal.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
+import NavHeader from "../components/navbar/NavHeader";
 
 function Legal() {
   
 return (
-    <div className="h-screen flex bg-gray-bg1"> <Navbar/>
+    <div className="h-screen flex bg-gray-bg1"> <NavHeader user="None" title="" />
     <div className="termspad">
     <div class="termscontainer" >
     <h1  className="text-2xl font-medium text-primary mt-4 mb-12 text-left">
