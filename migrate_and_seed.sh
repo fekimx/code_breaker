5ab3b3164d8833d5f16e7b2a99cf5a7791936084
Sean Bollin <sean@sean-bollin.com>
2022-04-30 19:07:48 -0700

    Fixing some of Romans installation feedback

--- migrate_and_seed.sh
+++ migrate_and_seed.sh
@@ -2,10 +2,10 @@
 
 # copied from https://medium.com/@ardho/migration-and-seeding-in-django-3ae322952111
 
-python3 manage.py migrate
+python manage.py migrate
 fixtures=$(ls seed/)
 while IFS= read -r fixture; do
     echo -n "Seeding "
     echo $fixture
-    python3 manage.py loaddata seed/$fixture
+    python manage.py loaddata seed/$fixture
 done <<< "$fixtures"
